93773cabb32068869b20468fc7b64ecc3e5f1a5e
David Amouzou <davidamzou@gmail.com>
2024-12-12 08:53:23 +0100

    Update mapRecipes function to accept 'any' type for data input, temporarily disabling ESLint for explicit any usage

--- app/data/cook_api/mapper.ts
+++ app/data/cook_api/mapper.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Recipe from "../model/recipe_model";
 
-export function mapRecipes(data: unknown): Recipe[] {
+export function mapRecipes(data: any): Recipe[] {
     const recipes: Recipe[] = [];
     for (const jsonData of data) {
         const recipe = Recipe.fromJson(jsonData);
